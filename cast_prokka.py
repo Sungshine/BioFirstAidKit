b78a5695b6697c09dd5a0336b449a1e078a7fcd5
wla9 <wla9@monolith1.biotech.cdc.gov>
2016-09-21 13:30:26 -0400

    script loads modules now

--- cast_prokka.py
+++ cast_prokka.py
@@ -10,22 +10,35 @@ import subprocess
 __author__  = "Sung Im"
 __email__   = "wla9@cdc.gov"
 
-# Load necessary modules.
 
-subprocess.open(["modulecmd", ""])
+# Load environment modules.
+execfile("/usr/share/Modules/init/python.py")
+module("load", "prokka")
+module("unload", "tbl2asn/22.4")
+module("load", "tbl2asn/25")
+module("unload", "hmmer/2.3.2")
+module("load", "hmmer/3.1b1")
+module("load", "hmmer/3.1b1")
+module("list")
 
+## Test directory with only one file
+# input_dir = "/scicomp/home/wla9/Projects/PooPrimer/data/ref_set/old"
+input_dir = "/scicomp/groups/OID/NCEZID/DFWED/EDLB/share/out/Calculation_Engine/STEC/PacBio"
+out_dir = "/scicomp/groups/OID/NCEZID/DFWED/EDLB/share/projects/CIMS/Escherichia/prokka/tmp/"
+paths = [os.path.join(input_dir, fn) for fn in next(os.walk(input_dir))[2]]
 
-# input_dir = "/scicomp/groups/OID/NCEZID/DFWED/EDLB/share/out/Calculation_Engine/STEC/PacBio"
-#
-# paths = [os.path.join(input_dir, fn) for fn in next(os.walk(input_dir))[2]]
-#
-# for path in paths:
-#     if ".fsa" or ".fasta" or ".fna" in os.path.basename(path):
-#         # print os.path.basename(path)
-#         filename = os.path.basename(path)
-#         subprocess.call(["prokka",
-#                          "-outdir", filename+".prokka",
-#                          "--prefix", filename,
-#                          "--force",
-#                          "--cpus", "10",
-#                          "--compliant"])
+for filepath in paths:
+    if ".fsa" or ".fasta" or ".fna" in os.path.basename(filepath):
+        print("Now processing {}.".format(os.path.basename(filepath)))
+        filename = os.path.basename(filepath)
+        subprocess.call(["prokka",
+                         "-outdir", out_dir + filename + ".prokka",
+                         "--prefix", filename,
+                         "--force",
+                         "--cpus", "10",
+                         "--compliant",
+                         filepath,
+                         ])
+
+print("Job complete. Purging modules.")
+module("purge")
