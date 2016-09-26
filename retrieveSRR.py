c2461f038f94a52349b1a7eff6404f3b61dee78d
wla9 <wla9@monolith1.biotech.cdc.gov>
2016-09-26 18:36:04 -0400

    given PNUSA ids, pipes ncbi eutil executables to return corresponding SRR ids. Still under construction.

--- retrieveSRR.py
+++ retrieveSRR.py
@@ -1,29 +1,62 @@
-__author__ = 'Sung Im'
-#!/usr/bin/env python
-import os
+#!/usr/bin/env python2.7
+
+""" Given a list of PNUSA Ids return the SRR Id associated, assuming the SRR Id exists.
+
+    NCBI eUtils executables are required in your system $PATH.
+
+    Usage:
+    python retrieveSRR.py [path to csv file containing PNUSA Ids]
+"""
+
 import sys
-import csv
 import subprocess
 
-filePath = sys.argv[1]
-edirectPath = sys.argv[2]
+__author__  = "Sung Im"
+__email__   = "wla9@cdc.gov"
 
-pulsenetList = []
+filePath = sys.argv[0]
 
-with open(filePath, 'rb') as csvfile:
+# Test case with pre-loaded list of PNUSA Ids.
+# pulsenet_ids = ["PNUSAL000100", "PNUSAL000101",]
 
-    readerObject = csv.reader(csvfile, delimiter=' ', quotechar='|')
+pulsenet_ids = []
 
-    for row in readerObject:
+# with open(filePath, 'rb') as csvfile:
+#
+#     readerObject = csv.reader(csvfile, delimiter=' ', quotechar='|')
+#
+#     for row in readerObject:
+#
+#         pulsenetKey = ', '.join(row)    # remove the brackets and quotation from list elements
+#         pulsenetList.append(pulsenetKey)
 
-        pulsenetKey = ', '.join(row)    # remove the brackets and quotation from list elements
-        pulsenetList.append(pulsenetKey)
+for key in pulsenet_ids:
 
-for key in pulsenetList:
+    esearch = subprocess.Popen(('esearch', '-db', 'biosample', '-query', key,),
+                               stdout=subprocess.PIPE,
+                               )
 
-    esearch = subprocess.call([edirectPath+'esearch', '-db', 'biosample', '-query', key,])
-    elink = subprocess.call([edirectPath+'elink', '-target', 'sra', '-db', 'sra',])
-    efetch = subprocess.call([edirectPath+'efetch', '-format', 'xml',])
-    xtract = subprocess.call([edirectPath+'xtract', '-pattern', 'IDENTIFIERS', '-element', 'PRIMARY_ID'])
-    grep = subprocess.call(['grep', 'SRR'])
+    elink = subprocess.Popen(("elink", "-target", "sra", "-db", "sra",),
+                             stdin=esearch.stdout,
+                             stdout=subprocess.PIPE,
+                             )
+    esearch.stdout.close()
 
+    efetch = subprocess.Popen(("efetch", "-format", "xml"),
+                              stdin=elink.stdout,
+                              stdout=subprocess.PIPE,
+                              )
+    elink.stdout.close()
+
+    xtract = subprocess.Popen(("xtract", "-pattern", "IDENTIFIERS", "-element", "PRIMARY_ID"),
+                              stdin=efetch.stdout,
+                              stdout=subprocess.PIPE,
+                              )
+    efetch.stdout.close()
+
+    sauce = subprocess.Popen(("grep", "SRR"),
+                             stdin=xtract.stdout)
+    xtract.stdout.close()
+
+## TODO need to figure out how to put output into a csv format - maybe use "communicate"
+## TODO need to figure out error handling for ID's that return nothing
