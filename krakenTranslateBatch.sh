6f4d7d8f85e1aebc334c4ccd2310bb1b160e1292
sim <sungbin401@gmail.com>
2016-10-25 17:40:03 -0400

    moved to CIMS folder

--- krakenTranslateBatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## Usage: run this script directly passing it your target directory
-## as the first argument, and it will iterate over the files in the
-## target folder and pass them as a parameter to krakenTranslate.sh
-
-for file in $1/*.sequence.kraken
-do qsub -M $2 /scicomp/home/wla9/Projects/CIMS/scripts/krakenTranslate.sh "$file"
-done
--- prokkabatch.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-## Usage: run this script directly passing it your target directory
-## as the first argument, and it will iterate over the files in the
-## target folder and pass them as a parameter to prokkaSingle.sh
-
-for file in $1/*
-do qsub -M $2 /scicomp/home/wla9/Projects/CIMS/scripts/prokkasingle.sh "$file"
-done
