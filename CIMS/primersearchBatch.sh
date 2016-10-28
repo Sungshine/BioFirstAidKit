c5f9473a63ba2e6c8f3698f589305cf17ea21b2e
wla9 <wla9@login01.aspen.biotech.cdc.gov>
2016-10-28 17:36:43 -0400

    eh

--- CIMS/primersearchBatch.sh
+++ CIMS/primersearchBatch.sh
@@ -4,6 +4,6 @@
 ## as the first argument, and it will iterate over the files in the
 ## target folder and pass them as a parameter to primersearchSingle.sh
 
-for file in $1/*
+for file in $1/*.fasta
 do qsub -M $2 /scicomp/home/wla9/Projects/CIMS/scripts/primersearchSingle.sh "$file"
 done
