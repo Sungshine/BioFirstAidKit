601aee4598442527978e5fd7c0db66b4f113bc31
sungshine <sungbin401@gmail.com>
2016-12-07 08:59:02 -0500

    ncbi api requests working.

--- eutils_helper.py
+++ eutils_helper.py
@@ -11,12 +11,44 @@ __author__ = 'Sung Im'
 __email__ = 'wla9@cdc.gov'
 __version__ = '0.1'
 
-# payload = {'db': 'biosample', 'query': 'PNUSAL000100'}
-response = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=biosample&term=PNUSAL000100")
-root = ElementTree.fromstring(response.content)
 
+esearch_resp = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=json&db=biosample&term=PNUSAL000100&usehistory=y").json()
+
+id = esearch_resp['esearchresult']['idlist'][0]     #2347439
+webenv = esearch_resp['esearchresult']['webenv']
+querykey = esearch_resp['esearchresult']['querykey']
+
+elink_resp = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?retmode=json&dbfrom=biosample&db=sra&query_key={}&WebEnv={}'.format(querykey, webenv, id)).json()
+
+link = elink_resp['linksets'][0]['linksetdbs'][0]['links'][0]   #491766
+
+efetch_resp = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&id={}'.format(link))
+
+# print(efetch_resp.content)
+tree = ElementTree.fromstring(efetch_resp.content)
+# for child in tree.iter('PRIMARY_ID'):
+#     print(child[1])
+
+for i in tree.findall('EXPERIMENT'):
+    print(i)
+
+
+
+
+
+
+
+
+# payload = {'db': 'biosample', 'query': 'PNUSAL000100'}
+
+# root = ElementTree.fromstring(response.content)
 # print(root)
 # for child in root:
 #     print(child.tag)
 
-print(root[0].text)
+# for child in root:
+#     print(child, child.attrib)
+
+# for elem in root.iter(tag='Count'):
+#     print(elem)
+
