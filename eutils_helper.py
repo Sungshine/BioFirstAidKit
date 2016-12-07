764c28b064965d127c7f1ed45c4abd24232ec399
sim <sungbin401@gmail.com>
2016-12-07 11:46:00 -0500

    replaced the json conversions back to xml - and parsing that way - it never works out when you try to take short cuts sung...

--- eutils_helper.py
+++ eutils_helper.py
@@ -3,7 +3,7 @@
 """ SRR Id retrieval
 """
 
-
+import csv
 import requests
 from xml.etree import ElementTree
 
@@ -12,43 +12,110 @@ __email__ = 'wla9@cdc.gov'
 __version__ = '0.1'
 
 
-esearch_resp = requests.get("https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?retmode=json&db=biosample&term=PNUSAL000100&usehistory=y").json()
+# globals
+esearch_base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi'
+elink_base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi'
+efetch_base = 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi'
 
-id = esearch_resp['esearchresult']['idlist'][0]     #2347439
-webenv = esearch_resp['esearchresult']['webenv']
-querykey = esearch_resp['esearchresult']['querykey']
+# Read in csv file containing PulseNet WGS_Ids
+# with open('/home/sim/Projects/PulseNetThings/Listeria_SRA_lookup_20160926.csv', 'rb') as csvfile:
+#     reader = csv.reader(csvfile, delimiter=',')
+#     next(reader, None)  # skips the header
+#     for row in reader:
+#         id = row[0]
 
-elink_resp = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/elink.fcgi?retmode=json&dbfrom=biosample&db=sra&query_key={}&WebEnv={}'.format(querykey, webenv, id)).json()
+# testlist = ['FDA00007715', 'CFSAN003732']
+testlist = ['FDA00007715']
+# testlist = ['PNUSAL000100']
+for id in testlist:
+    esearch_resp = requests.get(esearch_base
+                                +'?retmode=xml&db=biosample&term={}&usehistory=y'
+                                .format(id))
+    # print(esearch_resp.content)
+    s_tree = ElementTree.fromstring(esearch_resp.content)
+    for s_node in s_tree.iter('IdList'):
+        try:
+            s_id = s_node[0].text
+            # print(s_id)
+        except IndexError, KeyError:
+            print(id, "WGS Id not found.")
 
-link = elink_resp['linksets'][0]['linksetdbs'][0]['links'][0]   #491766
+        elink_resp = requests.get(elink_base + '?retmode=xml&dbfrom=biosample&db=sra&id={}'.format(s_id))
+        # print(elink_resp.content)
 
-efetch_resp = requests.get('https://eutils.ncbi.nlm.nih.gov/entrez/eutils/efetch.fcgi?db=sra&id={}'.format(link))
+        e_tree = ElementTree.fromstring(elink_resp.content)
+        for e_node in e_tree.findall('LinkSet'):
 
-# print(efetch_resp.content)
-tree = ElementTree.fromstring(efetch_resp.content)
-# for child in tree.iter('PRIMARY_ID'):
-#     print(child[1])
+            link = e_node.find('LinkSetDb')
+            # If no links exist, move on to next id
+            if link is None:
+                print(id, 'No links exist.')
+                continue
+            else:
+                link = link.find('Link')
+                e_id = link[0].text
 
-for i in tree.findall('EXPERIMENT'):
-    print(i)
-
-
-
-
-
-
+                efetch_resp = requests.get(efetch_base + '?db=sra&id={}'.format(e_id))
 
 
-# payload = {'db': 'biosample', 'query': 'PNUSAL000100'}
+        # for e_node in e_tree.iter('Link'):
+        #     if e_node == '':
+        #         print(id, "No links out.")
+            # try:
+            #     e_id = e_node[0].text
+            #     print(e_node[0].text)
+            # except IndexError, KeyError:
+            #     print(id, 'No out links exist.')
+            # if not e_id:
+            #     print('No e_id.')
 
-# root = ElementTree.fromstring(response.content)
-# print(root)
-# for child in root:
-#     print(child.tag)
+            # efetch_resp = requests.get(efetch_base + '?db=sra&id={}'.format(e_id))
+            # # print(efetch_resp.content)
+            # f_tree = ElementTree.fromstring(efetch_resp.content)
+            # for node in f_tree.iter('IDENTIFIERS'):
+            #     # print(node[0].text)
+            #     try:
+            #         primary_id = node[0].text
+            #     except IndexError, KeyError:
+            #         print(id, 'No SRR id exists')
+            #     if not primary_id:
+            #         print('No primary_id.')
+            #     if 'SRR' in primary_id:
+            #         print(primary_id)
+            #     else:
+            #         print(id, 'No SRR Id exists.')
+            #         continue
 
-# for child in root:
-#     print(child, child.attrib)
 
-# for elem in root.iter(tag='Count'):
-#     print(elem)
+    # try:
+    #     id = esearch_resp['esearchresult']['idlist'][0]
+    #     webenv = esearch_resp['esearchresult']['webenv']
+    #     querykey = esearch_resp['esearchresult']['querykey']
+    # except IndexError:
+    #     print(id, " PNUSA Id not found.")
+    #     continue
+    #     # pass
 
+    # elink_resp = requests.get(elink_base +
+    #                           '?retmode=json&dbfrom=biosample&db=sra&query_key={}&WebEnv={}'
+    #                           .format(querykey, webenv, id)).json()
+    # print(elink_resp)
+    # try:
+    #     link = elink_resp['linksets'][0]['linksetdbs'][0]['links'][0]
+    #     print(link)
+    # except IndexError, KeyError:
+    #     print(id, " No links out.")
+    #     continue
+    #     # pass
+
+    # efetch_resp = requests.get(efetch_base + '?db=sra&id={}'.format(link))
+    #
+    # tree = ElementTree.fromstring(efetch_resp.content)
+    #
+    # for node in tree.iter('IDENTIFIERS'):
+    #     primary_id = node[0].text
+    #     if 'SRR' in primary_id:
+    #         print(primary_id)
+    #     else:
+    #         continue
+    #         # pass
