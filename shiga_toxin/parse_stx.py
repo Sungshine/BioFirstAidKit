00891d6e29d24a9f0534a1d306a1afdd0e58e4ed
Sung Im <sungbin401@gmail.com>
2017-01-31 10:49:06 -0500

    meh

--- shiga_toxin/parse_stx.py
+++ shiga_toxin/parse_stx.py
@@ -17,28 +17,25 @@ dir_path = os.getcwd()
 file_path = sys.argv[1]
 filein = open(file_path, 'r')
 
+out1 = open(dir_path+'/stx1.fsa', 'wa')
+out2 = open(dir_path+'/stx2.fsa', 'wa')
+
 for line in filein:
+
     if line.startswith('>stx1'):
-        if line.startswith('>'):
-            fmt_file_id = line.strip('>').rstrip()
-            out_file = 'stx1.fsa'
-            # open outfile handle
-            out = open(dir_path + '/{}'.format(out_file), 'wa')
-            # write header
-            out.write(line)
-        else:
-            if line != '':
-                out.write(line)
-            else:
-                out.close()
-    elif line.startswith('>stx2'):
-        if line.startswith('>'):
-            fmt_file_id = line.strip('>').rstrip()
-            out_file = 'stx2.fsa'
-            out = open(dir_path + '/{}'.format(out_file), 'wa')
-            out.write(line)
-        else:
-            if line != '':
-                out.write(line)
-            else:
-                out.close()
+        out1.write(line)
+        continue
+    if line != '':
+        out1.write(line)
+    if line == '':
+        continue
+    if line.startswith('>stx2'):
+        out2.write(line)
+        continue
+    if line != '':
+        out2.write(line)
+    if line == '':
+        continue
+
+out1.close()
+out2.close()
