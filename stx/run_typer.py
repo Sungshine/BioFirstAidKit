22c870d9da83860f3835645949c615fa9ce57a35
Sung Im <sungbin401@gmail.com>
2017-02-07 21:08:43 -0500

    testing stderr to stdout as well

--- stx/run_typer.py
+++ stx/run_typer.py
@@ -73,8 +73,13 @@ if __name__ == '__main__':
         outname = os.path.basename(key).split('_')[0]
         outfile = '{}/{}.txt'.format(out_directory, outname)
         out_handle = open(outfile, 'w')
+
         ps = subprocess.Popen((wall_timer, '-v', 'typer', '-x', database, '-1', r1, '-2', r2, '-k', args.kmer),
-                              stdout=subprocess.PIPE)
-        output = ps.communicate()[0]
+                              stderr=subprocess.STDOUT,
+                              stdout=subprocess.PIPE,
+                              )
+
+        output = ps.communicate()
+
         out_handle.write(str(output))
         out_handle.close()
