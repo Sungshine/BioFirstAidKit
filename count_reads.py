ce3ca444a20cf75fcb5710f34ac3737ab988d6ff
Sung Im <sungbin401@gmail.com>
2017-02-16 20:26:42 -0500

    sequence reads counting script

---
+++ count_reads.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python2.7
+
+""" Calculate number of reads in fastq file.
+
+"""
+
+
+import os
+import errno
+import argparse
+import subprocess
+
+
+__author__ = 'Sung Im'
+__email__ = 'sungbin401@gmail.com'
+
+
+if __name__ == '__main__':
+
+
+    # Command line arguments.
+    opt_parse = argparse.ArgumentParser(description='Launch STing Utilities on a directory of reads.')
+    opt_parse.add_argument('-i', '--input-directory', dest='indir', required=True,
+                           help='Path to directory containing paired read files.')
+    opt_parse.add_argument('-d', '--database', dest='db', required=True, help='Path to database + prefix.')
+    opt_parse.add_argument('-o', '--outdir', dest='outdir', required=True, help='Path to results directory.')
+    args = opt_parse.parse_args()
+
+    # Designate pointers to absolute paths.
+    read_paths = [os.path.join(args.indir, fn) for fn in next(os.walk(args.indir))[2]]
+
+    outfile = '{}/readcounts.csv'.format(args.outdir)
+    outhandle = open(outfile, 'wa')
+
+    for file in read_paths:
+
+        filename = os.path.basename(file)
+        filesize = os.stat(file).st_size
+
+        cat = subprocess.Popen(('cat', file), stdout=subprocess.PIPE)
+        grep = subprocess.Popen(('grep', '\@'), stdin=cat.stdout, stdout=subprocess.PIPE)
+        count = subprocess.Popen(('wc', '-l'), stdin=grep.stdout, stdout=subprocess.PIPE)
+
+        out = count.stdout
+
+        outfile.write('{},{},{}'.format(filename, out, filesize))
+
+    outfile.close()
+
+
+
